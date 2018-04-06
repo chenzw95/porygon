6c41ff850e2081604085516aa814a7aed3cb8edc
chenzw <chenzw25@gmail.com>
2018-04-06 18:43:50 +0800

    userinfo command, and aliases

--- cogs/debug.py
+++ cogs/debug.py
@@ -48,7 +48,7 @@ class Debug:
             await ctx.send("⚠ Operation failed!\n```\n{}: {}```".format(type(e).__name__, e))
             logger.exception(e)
 
-    @commands.command()
+    @commands.command(aliases=['ri'])
     @commands.guild_only()
     async def roleinfo(self, ctx, role: discord.Role):
         embed = discord.Embed(title="Role information", color=role.color, timestamp=ctx.message.created_at)
@@ -56,13 +56,35 @@ class Debug:
         embed.add_field(name="Hoisted", value=role.hoist, inline=False).add_field(name="Mentionable", value=role.mentionable)
         await ctx.send(embed=embed)
 
+    @commands.command(aliases=['ui'])
+    @commands.guild_only()
+    async def userinfo(self, ctx, raw_member=None):
+        if not raw_member:
+            member = ctx.author
+        else:
+            try:
+                member = await commands.MemberConverter().convert(ctx, raw_member)
+            except commands.BadArgument:
+                return await ctx.send("❌ User not found. Search terms are case sensitive.")
+        embed = discord.Embed(title="{} ({})".format(member, member.display_name), color=member.top_role.color, timestamp=ctx.message.created_at)
+        embed.set_author(name="User information")
+        embed.add_field(name="ID", value=member.id).add_field(name="Status", value=member.status)
+        embed.add_field(name="Activity", value=member.activity)
+        embed.add_field(name="Roles", value=", ".join([str(role) for role in member.roles[1:]]), inline=False)
+        embed.add_field(name="Created at", value=member.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+        embed.add_field(name="Joined at", value=member.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+        await ctx.send(embed=embed)
+
     @roleinfo.error
+    @userinfo.error
     async def info_handler(self, ctx, error):
         error = getattr(error, 'original', error)
         if isinstance(error, discord.ext.commands.BadArgument):
-            await ctx.send("❌ Object not found.")
+            await ctx.send("❌ Object not found. Search terms are case sensitive.")
         elif isinstance(error, discord.ext.commands.NoPrivateMessage):
             await ctx.send("⚠ This command must be executed in a server!")
+        else:
+            raise error  # This will print to console (only)
 
 
 def setup(bot):
