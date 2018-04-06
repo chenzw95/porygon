567b38de53908cc41eab603dda93cf29ae3159bf
chenzw <chenzw25@gmail.com>
2018-04-06 13:52:42 +0800

    debug: check unload operation against new name

--- cogs/debug.py
+++ cogs/debug.py
@@ -39,7 +39,7 @@ class Debug:
     async def unload(self, ctx, module: str):
         if module[0:5] != "cogs.":
             module = "cogs." + module
-        if module == "cogs.cog_admin":
+        if module == "cogs.debug":
             return await ctx.send("⛔ You may not unload this!")
         try:
             self.bot.unload_extension(module)
@@ -47,6 +47,13 @@ class Debug:
         except Exception as e:
             await ctx.send("⚠ Operation failed!\n```\n{}: {}```".format(type(e).__name__, e))
             logger.exception(e)
+
+    @commands.command()
+    @commands.guild_only()
+    async def roleinfo(self, ctx, role: discord.Role):
+        embed = discord.Embed(title="Role information", color=role.color, timestamp=ctx.message.created_at)
+        embed.add_field(name="Name", value=role.name).add_field(name="ID", value=role.id)
+        embed.add_field(name="Hoisted", value=role.hoist, inline=False).add_field(name="Mentionable", value=role.mentionable)
 
 
 def setup(bot):
