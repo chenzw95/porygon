ba551a8a360485ef9639355d591dbc63e90481e6
chenzw <chenzw25@gmail.com>
2018-04-23 11:30:10 +0800

    change user to member for consistency with library

--- cogs/mod.py
+++ cogs/mod.py
@@ -13,58 +13,58 @@ class Mod:
     async def on_member_join(self, user):
         embed = discord.Embed(color=discord.Color.green())
         embed.title = "New member"
-        embed.add_field(name="User", value=user.mention)
-        embed.add_field(name="Joined at", value=user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
-        embed.add_field(name="Created at", value=user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+        embed.add_field(name="User", value=member.mention)
+        embed.add_field(name="Joined at", value=member.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+        embed.add_field(name="Created at", value=member.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
         await self.bot.modlog_channel.send(embed=embed)
 
-    async def on_member_remove(self, user):
+    async def on_member_remove(self, member):
         embed = discord.Embed(color=discord.Color.red())
         embed.title = "Member left"
-        embed.add_field(name="User", value=user.mention)
+        embed.add_field(name="User", value=member.mention)
         await self.bot.modlog_channel.send(embed=embed)
 
     @commands.command(name='promote', aliases=['addrole'])
     @commands.guild_only()
     @commands.has_any_role("Moderators")
-    async def promote(self, ctx, user: discord.Member, role: str):
+    async def promote(self, ctx, member: discord.Member, role: str):
         """Adds Builder/ GitHub Contributor"""
         if role.lower() in ["builder", "builders"]:
             newrole = discord.utils.get(ctx.guild.roles, name="Builders")
         if role.lower() in ["contributor", "contrib", "github"]:
             newrole = discord.utils.get(ctx.guild.roles, name="GitHub Contributors")
         if newrole:
-            if newrole not in user.roles:
+            if newrole not in member.roles:
                 embed = discord.Embed(color=discord.Color.gold(), timestamp=ctx.message.created_at)
                 embed.title = "Role added"
-                embed.add_field(name="Target", value=user.mention)
+                embed.add_field(name="Target", value=member.mention)
                 embed.add_field(name="Role", value=newrole.name)
                 embed.add_field(name="Action taken by", value=ctx.author.name)
-                await user.add_roles(newrole)
+                await member.add_roles(newrole)
                 embed.title = "Added {} role".format(newrole.name)
-                await ctx.send("{} : {} role has been added to you!".format(user.mention, newrole.name))
+                await ctx.send("{} : {} role has been added to you!".format(member.mention, newrole.name))
                 await self.bot.modlog_channel.send(embed=embed)
             else:
-                await ctx.send("{} : {} already has this role!".format(ctx.author.mention, user.name))
+                await ctx.send("{} : {} already has this role!".format(ctx.author.mention, member.name))
         else:
             await ctx.send("⚠ Unrecognised role!")
 
     @commands.command()
     @checks.check_permissions_or_owner(kick_members=True)
     @commands.bot_has_permissions(kick_members=True)
-    async def kick(self, ctx, user: discord.Member, *, reason: str = None):
-        """Kicks the user"""
+    async def kick(self, ctx, member: discord.Member, *, reason: str = None):
+        """Kicks a member."""
         author = ctx.message.author
         embed = discord.Embed(color=discord.Color.red(), timestamp=ctx.message.created_at)
         embed.title = "Kicked user"
-        embed.add_field(name="User", value=user.mention)
+        embed.add_field(name="User", value=member.mention)
         embed.add_field(name="Action taken by", value=ctx.author.name)
-        if user:
-            if author.top_role.position < user.top_role.position + 1:
+        if member:
+            if author.top_role.position < member.top_role.position + 1:
                 return await ctx.send("⚠ Operation failed!\nThis cannot be allowed as you are not above the member in role hierarchy.")
             else:
-                await user.kick(reason=reason)
-                return_msg = "Kicked user: {}".format(user.mention)
+                await member.kick(reason=reason)
+                return_msg = "Kicked user: {}".format(member.mention)
                 if reason:
                     return_msg += " for reason `{}`".format(reason)
                 return_msg += "."
@@ -74,19 +74,19 @@ class Mod:
     @commands.command()
     @checks.check_permissions_or_owner(ban_members=True)
     @commands.bot_has_permissions(ban_members=True)
-    async def ban(self, ctx, user:discord.Member, *, reason: str = None):
-        """Bans the user"""
+    async def ban(self, ctx, member: discord.Member, *, reason: str = None):
+        """Bans a member."""
         author = ctx.message.author
         embed = discord.Embed(color=discord.Color.red(), timestamp=ctx.message.created_at)
         embed.title = "Banned user"
-        embed.add_field(name="User", value=user.mention)
+        embed.add_field(name="User", value=member.mention)
         embed.add_field(name="Action taken by", value=ctx.author.name)
-        if user:
-            if author.top_role.position < user.top_role.position + 1:
+        if member:
+            if author.top_role.position < member.top_role.position + 1:
                 return await ctx.send("⚠ Operation failed!\nThis cannot be allowed as you are not above the member in role hierarchy.")
             else:
-                await user.ban(reason=reason)
-                return_msg = "Banned user: {}".format(user.mention)
+                await member.ban(reason=reason)
+                return_msg = "Banned user: {}".format(member.mention)
                 if reason:
                     return_msg += " for reason `{}`".format(reason)
                 return_msg += "."
@@ -98,7 +98,7 @@ class Mod:
     async def kick_handler(self, ctx, error):
         error = getattr(error, 'original', error)
         if isinstance(error, commands.BadArgument):
-            await ctx.send("User could not be found.")
+            await ctx.send("Member could not be found.")
         elif isinstance(error, commands.BotMissingPermissions):
             await ctx.send("⚠ I don't have the permissions to do this.")
         elif isinstance(error, commands.CheckFailure):
@@ -108,7 +108,7 @@ class Mod:
     async def promote_handler(self, ctx, error):
         error = getattr(error, 'original', error)
         if isinstance(error, commands.BadArgument):
-            await ctx.send("{} User could not be found! Usage of this command is as follows ```!promote <user> <role>```".format(ctx.message.author.mention))
+            await ctx.send("{} Member could not be found! Usage of this command is as follows ```!promote <member> <role>```".format(ctx.message.author.mention))
         elif isinstance(error, commands.CheckFailure):
             await ctx.send("{} You don't have permission to use this command.".format(ctx.message.author.mention))
 
