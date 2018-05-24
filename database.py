5755b4296abf13721969dfbe76cb96fe2ba89e30
Archit Date <architdate@gmail.com>
2018-05-25 06:34:35 +0800

    polling: extend github watch functionality
    Add code to add and remove repositories to watch. Update database schema to incorporate this.

--- database.py
+++ database.py
@@ -13,3 +13,8 @@ restrictions_tbl = sa.Table("restrictions", metadata,
 config_tbl = sa.Table("config", metadata,
                       sa.Column("name", sa.String(50), primary_key=True),
                       sa.Column("value", sa.String(100)))
+
+github_watch_tbl = sa.Table("github-watch", metadata,
+                      sa.Column("name", sa.String(100), nullable=False, primary_key=True),
+                      sa.Column("owner", sa.String(50), nullable=False, primary_key=True),
+                      sa.Column("commit", sa.String(50), nullable=False))
