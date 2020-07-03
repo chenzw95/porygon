05d203451cee8b522111e3891c71a5b40b779ebd
Archit Date <architdate@gmail.com>
2020-07-03 11:55:35 +0800

    add more assignables

--- cogs/assignables.py
+++ cogs/assignables.py
@@ -7,8 +7,32 @@ class Assignables(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='toggleupdates', aliases=['buildupdates'])
+    @commands.command(name='toggleplugins', aliases=['buildupdates'])
     async def buildupdates(self, ctx):
+        """Toggles the PluginsUpdates role"""
+        if not ctx.channel.id == 429185857346338827:
+            await ctx.message.delete()
+            try:
+                await ctx.author.send("This command can only be used in <#429185857346338827>. Please move there.")
+            except discord.Forbidden:
+                pass
+            return
+        burole = discord.utils.get(ctx.guild.roles, name="PluginsUpdates")
+        embed = discord.Embed(color=discord.Color.gold(), timestamp=ctx.message.created_at)
+        embed.add_field(name="User", value=ctx.message.author.mention)
+        if burole not in ctx.author.roles:
+            await ctx.author.add_roles(burole)
+            embed.title = "Added PluginsUpdates role"
+            await ctx.send("{} : Added Plugin Updates role! You will be notified from now on when a new plugin build is released!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed)
+        else:
+            await ctx.author.remove_roles(burole)
+            embed.title = "Removed PluginsUpdates role"
+            await ctx.send("{} : Removed Plugin Updates role! You will no longer be notified from now on when a new plugin build is released!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed)
+            
+    @commands.command(name='togglesysbot')
+    async def sysbotupdates(self, ctx):
         """Toggles the BuildUpdates role"""
         if not ctx.channel.id == 429185857346338827:
             await ctx.message.delete()
@@ -17,19 +41,67 @@ class Assignables(commands.Cog):
             except discord.Forbidden:
                 pass
             return
-        burole = discord.utils.get(ctx.guild.roles, name="BuildUpdates")
+        burole = discord.utils.get(ctx.guild.roles, name="SysBotUpdates")
         embed = discord.Embed(color=discord.Color.gold(), timestamp=ctx.message.created_at)
         embed.add_field(name="User", value=ctx.message.author.mention)
         if burole not in ctx.author.roles:
             await ctx.author.add_roles(burole)
-            embed.title = "Added BuildUpdates role"
-            await ctx.send("{} : Added Build Updates role! You will be notified from now on when a new build is released!".format(ctx.author.mention))
+            embed.title = "Added SysBotUpdates role"
+            await ctx.send("{} : Added SysBot Updates role! You will be notified of any major SysBot news from now on!".format(ctx.author.mention))
             await self.bot.modlog_channel.send(embed=embed)
         else:
             await ctx.author.remove_roles(burole)
-            embed.title = "Removed BuildUpdates role"
-            await ctx.send("{} : Removed Build Updates role! You will no longer be notified from now on when a new build is released!".format(ctx.author.mention))
+            embed.title = "Removed SysBotUpdates role"
+            await ctx.send("{} : Removed SysBot Updates role! You will no longer be notified of SysBot news!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed) 
+            
+    @commands.command(name='togglenhse')
+    async def nhseupdates(self, ctx):
+        """Toggles the NHSEUpdates role"""
+        if not ctx.channel.id == 429185857346338827:
+            await ctx.message.delete()
+            try:
+                await ctx.author.send("This command can only be used in <#429185857346338827>. Please move there.")
+            except discord.Forbidden:
+                pass
+            return
+        burole = discord.utils.get(ctx.guild.roles, name="NHSEUpdates")
+        embed = discord.Embed(color=discord.Color.gold(), timestamp=ctx.message.created_at)
+        embed.add_field(name="User", value=ctx.message.author.mention)
+        if burole not in ctx.author.roles:
+            await ctx.author.add_roles(burole)
+            embed.title = "Added NHSEUpdates role"
+            await ctx.send("{} : Added NHSE Updates role! You will be notified of any major NHSE news from now on!".format(ctx.author.mention))
             await self.bot.modlog_channel.send(embed=embed)
+        else:
+            await ctx.author.remove_roles(burole)
+            embed.title = "Removed NHSEUpdates role"
+            await ctx.send("{} : Removed NHSE Updates role! You will no longer be notified of NHSE news!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed) 
+            
+    @commands.command(name='togglepkhex')
+    async def pkhexupdates(self, ctx):
+        """Toggles the PKHeXUpdates role"""
+        if not ctx.channel.id == 429185857346338827:
+            await ctx.message.delete()
+            try:
+                await ctx.author.send("This command can only be used in <#429185857346338827>. Please move there.")
+            except discord.Forbidden:
+                pass
+            return
+        burole = discord.utils.get(ctx.guild.roles, name="PKHeXUpdates")
+        embed = discord.Embed(color=discord.Color.gold(), timestamp=ctx.message.created_at)
+        embed.add_field(name="User", value=ctx.message.author.mention)
+        if burole not in ctx.author.roles:
+            await ctx.author.add_roles(burole)
+            embed.title = "Added PKHeXUpdates role"
+            await ctx.send("{} : Added PKHeX Updates role! You will be notified of any major PKHeX news from now on!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed)
+        else:
+            await ctx.author.remove_roles(burole)
+            embed.title = "Removed PKHeXUpdates role"
+            await ctx.send("{} : Removed PKHeX Updates role! You will no longer be notified of PKHeX news!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed) 
 
 def setup(bot):
     bot.add_cog(Assignables(bot))
