da31e2f98b2c6e1cab2099109e3d647606e93736
Griffin <4n0nym0ush4ck3r666@gmail.com>
2020-09-15 18:53:20 -0600

    add togglegames (#21)

--- cogs/assignables.py
+++ cogs/assignables.py
@@ -127,7 +127,7 @@ class Assignables(commands.Cog):
             embed.title = "Removed the weeb role"
             await ctx.send("{} : Removed the weeb role! You will no longer be able to access the weeb-spoilers channel!".format(ctx.author.mention))
             await self.bot.modlog_channel.send(embed=embed)
-            
+ 
     @commands.command(name='togglemovies')
     async def moviesrole(self, ctx):
         """Toggles the Movie Nights! role"""
@@ -151,6 +151,31 @@ class Assignables(commands.Cog):
             embed.title = "Removed the Movie Nights! role"
             await ctx.send("{} : Removed the Movie Nights! role! You will no longer be notified of community movie nights!".format(ctx.author.mention))
             await self.bot.modlog_channel.send(embed=embed)
+
+    @commands.command(name='togglegames')
+    async def gamesrole(self, ctx):
+        """Toggles the Movie Nights! role"""
+        if not ctx.channel.id == 429185857346338827:
+            await ctx.message.delete()
+            try:
+                await ctx.author.send("This command can only be used in <#429185857346338827>. Please move there.")
+            except discord.Forbidden:
+                pass
+            return
+        burole = discord.utils.get(ctx.guild.roles, name="Community Games")
+        embed = discord.Embed(color=discord.Color.gold(), timestamp=ctx.message.created_at)
+        embed.add_field(name="User", value=ctx.message.author.mention)
+        if burole not in ctx.author.roles:
+            await ctx.author.add_roles(burole)
+            embed.title = "Added the Community Games role"
+            await ctx.send("{} : Added the Community Games role! You will be notified of community games!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed)
+        else:
+            await ctx.author.remove_roles(burole)
+            embed.title = "Removed the Community Games role"
+            await ctx.send("{} : Removed the Community Games role! You will no longer be notified of community games!".format(ctx.author.mention))
+            await self.bot.modlog_channel.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Assignables(bot))
