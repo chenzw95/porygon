18e94c1929665c471711a12eef02641f43f48bde
Archit Date <architdate@gmail.com>
2021-05-19 21:55:31 +0800

    make welcome messages more readable

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -10,7 +10,7 @@ class Welcome(commands.Cog):
     async def on_member_join(self, user):
         welcome = discord.Embed(color=discord.Color.gold())
         welcome.title = "Welcome to the PKHeX Development Projects server, {}!".format(user.name)
-        welcome.description = "Don't forget to read #rules and #announcements. Also if you are interested in the `@PluginsUpdates`, `@SysBotUpdates`, `@PKHeXUpdates` or `@NHSEUpdates` role, use the `!toggleplugins`, `!togglesysbot`, `!togglepkhex`, or `!togglenhse` commands respectively to get notifications on the latest news or builds!"
+        welcome.description = "Don't forget to read <#401014737040834570> and <#438356161218215937> before you start, and keep an eye on <#401017356333088771> for updates!\n\nIf you want to be notified of updates, add the appropriate role in #bot-testing.\n- PKHeX-Plugins (`!toggleplugins`)\n- SysBot.NET (`!togglesysbot`)\n- NHSE / SysBot.AC (`!togglenhse`)"
         await self.bot.welcome_channel.send(user.mention, embed=welcome)
 
 
